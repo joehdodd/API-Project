0245487d3a56d07d313173a46ad94530fc3e16e7
Joe Dodd <joehdodd@gmail.com>
2017-04-15 19:59:16 -0400

    Template literals and such

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@ $('document').ready(function () {
 
   const form = document.getElementById('searchForm');
   const searchCon = document.getElementById('searchContainer');
-  let searchText = document.getElementById('searchField');
-
+  const searchText = document.getElementById('searchField');
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -13,24 +12,23 @@ $('document').ready(function () {
         if (xhr.status === 200) {
           let response = JSON.parse(xhr.responseText);
           let results = response.albums.items;
+          let responseHTML = `<div id="searchContainer" class="container gallery">`
           for (let i = 0; i < results.length; ++i) {
             let albumName = results[i].name;
             let albumImage = results[i].images[0].url;
-            let responseHTML = `<div class="col">`;
-            let link = `<a href="` +albumImage+ `" data-lightbox="gallery" data-title="` +albumName+ `">`;
-            responseHTML += link;
-            let img = `<img class="gallery-item" src="` +albumImage+ `" title="Album Cover">`;
-            responseHTML += img;
-            responseHTML += `</a>`
-            responseHTML += `</div>`;
-            searchCon.innerHTML = responseHTML;
+            responseHTML += `<div class="flex">
+                               <a href="` +albumImage+ `" data-lightbox="gallery" data-title="` +albumName+ `">
+                               <img class="gallery-item" src="` +albumImage+ `" title="` +albumName+ `"></a>
+                             </div>`;
           }
+          responseHTML += `</div>`
+          searchCon.innerHTML = responseHTML;
           console.log(response);
         }
       }
     };
-    let searchQuery = `?q=`+searchText.value+`&type=album&limit=8`;
-    xhr.open(`GET`, `https://api.spotify.com/v1/search`+searchQuery);
+    let searchQuery = '?q='+searchText.value+'&type=album&limit=8';
+    xhr.open('GET', 'https://api.spotify.com/v1/search'+searchQuery);
     xhr.send();
   }); // end submit
 }); // end ready
