17d4fd31d3e990920f933e43c39449241770a248
Joe Dodd <joehdodd@gmail.com>
2017-04-15 19:15:26 -0400

    Deletion and such

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 $('document').ready(function () {
 
   const form = document.getElementById('searchForm');
-  const searchCon = null;
+  const searchCon = document.getElementById('searchContainer');
   let searchText = document.getElementById('searchField');
+
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -13,18 +14,18 @@ $('document').ready(function () {
           let response = JSON.parse(xhr.responseText);
           let results = response.albums.items;
           for (let i = 0; i < results.length; ++i) {
-            let responseHTML = `<div class="col">`;
             let albumImage = results[i].images[0].url;
             let albumName = results[i].name;
             let albumURL = results[i].external_urls.spotify;
             let albumArtist = results[i].artists[0].name;
-            let link = `<a href="` +albumImage+ `"` +` data-lightbox="gallery" data-title="` +albumName+ `">`;
+            let responseHTML = `<div class="col">`;
+            let link = `<a href="` +albumImage+ `" data-lightbox="gallery" data-title="` +albumName+ `">`;
             responseHTML += link;
-            let img = `<img class="gallery-item" src="`+albumImage+ `"` +`title="Album Cover">`;
+            let img = `<img class="gallery-item" src="` +albumImage+ `" title="Album Cover">`;
             responseHTML += img;
             responseHTML += `</a>`
             responseHTML += `</div>`;
-            document.getElementById('searchContainer').innerHTML = responseHTML;
+            searchCon.innerHTML = responseHTML;
           }
           console.log(response);
         }
@@ -33,5 +34,5 @@ $('document').ready(function () {
     let searchQuery = '?q='+searchText.value+'&type=album&limit=12';
     xhr.open('GET', 'https://api.spotify.com/v1/search'+searchQuery);
     xhr.send();
-  });// end submit
+  }); // end submit
 }); // end ready
